067867fc4ff02f6b4386b4afb423aeeb7ec1b73f
Leonid Morose <leonid.morose@gmail.com>
2012-10-11 22:11:14 +0400

    аналог join 2

--- models/user.js
+++ models/user.js
@@ -4,14 +4,13 @@ var Schema = mongoose.Schema;
 
 // create User model
 var User = new Schema({
-  username: {type:String, require: true, trim: true, unique: true},
+  email: { type: String, required: true, index: { unique: true, sparse: true }},
   password: String,
+  alive: Boolean,
   dateCreated: {type:Date, default: Date.now},
   updated: {type:Date, default: Date.now},
-  profile: {
-    firstName: String,
-    lastName: String
-  }
+  // вакансии
+  vacancies : [{ type: Schema.Types.ObjectId, ref: 'Vacancy' }]
 });
 
 module.exports = mongoose.model('User', User);
